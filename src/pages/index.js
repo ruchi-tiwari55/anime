18dbd786c7a89e73ebca9653554718b7c5e173d3
Ruchi <ruchit215@gmail.com>
2022-09-30 20:42:08 +0530

    task: filter by genres feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import { Card, Input } from "antd";
+import { Card, Input, Select, Space } from "antd";
 import { useEffect, useState } from "react";
 
 function Home() {
   const [items, setItems] = useState([]);
   const [filteredItems, setFilteredItems] = useState([]);
+  const [genresTypes, setGenresTypes] = useState([]);
 
   useEffect(() => {
     fetch("https://api.jikan.moe/v4/anime")
@@ -12,6 +13,13 @@ function Home() {
       })
       .then((responseData) => {
         setItems(responseData.data);
+        let genresObj = responseData.data.reduce((obj, item) => {
+          item.genres.map((gen) => {
+            obj[gen.name] = 1;
+          });
+          return obj;
+        }, {});
+        setGenresTypes(Object.keys(genresObj));
         setFilteredItems(responseData.data);
       })
       .catch((error) => {
@@ -27,12 +35,30 @@ function Home() {
     setFilteredItems(filteredItems);
   };
 
+  const handleChange = (selectedValues) => {
+    if (selectedValues.length == 0) {
+      setFilteredItems(items);
+      return;
+    }
+    let filteredItems = items.filter((item) => {
+      let includeThisItem = false;
+      item.genres.map((gen) => {
+        if (selectedValues.includes(gen.name)) {
+          includeThisItem = true;
+        }
+      });
+      return includeThisItem;
+    });
+    setFilteredItems(filteredItems);
+  };
+
   return (
     <div style={{ backgroundColor: "lightgreen", minHeight: "100vh" }}>
       <div
         style={{
           width: "100%",
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           padding: 30,
@@ -46,6 +72,28 @@ function Home() {
             width: 300,
           }}
         />
+        <div style={{ width: "100%", marginTop: 15 }}>
+          <Space>
+            <div>Filter by Genres:</div>
+            <Select
+              mode="multiple"
+              allowClear
+              style={{
+                width: 200,
+              }}
+              placeholder="select genres"
+              onChange={handleChange}
+            >
+              {genresTypes.map((item, index) => {
+                return (
+                  <Select.Option key={index} value={item}>
+                    {item}
+                  </Select.Option>
+                );
+              })}
+            </Select>
+          </Space>
+        </div>
       </div>
       <div
         style={{
